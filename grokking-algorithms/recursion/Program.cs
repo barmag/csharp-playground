880a5bb0969f3e908e18d018e81e745910cd9881
Yasser Makram <yasser@barm.ag>
2017-08-19 14:59:28 +0200

    implement sum with recursion

---
+++ grokking-algorithms/recursion/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    class Program
+    {
+        static int sum(int[] elements)
+        {
+            if (elements.Length == 0)
+                return 0;
+            if (elements.Length == 1)
+                return elements[0];
+
+            return elements[0] + sum(elements.Skip(1).Take(elements.Length - 1).ToArray());
+        }
+        static void Main(string[] args)
+        {
+            var elements = Enumerable.Range(0, 10).ToArray();
+            
+            var result = sum(elements);
+        }
+    }
+}
