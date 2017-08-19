2aae86acef86bc55d07f7aea44691a3b7a2d336d
Yasser Makram <yasser@barm.ag>
2017-08-19 15:59:57 +0200

    binary search recursive

--- grokking-algorithms/recursion/Program.cs
+++ grokking-algorithms/recursion/Program.cs
@@ -38,11 +38,43 @@ namespace recursion
             var max = arr[0] > tempMax ? arr[0] : tempMax;
             return max;
         }
+
+        static int binarySearchRecursive(int qry, int[] array)
+        {
+            if (array.Length == 0)
+                return -1;
+            if (array.Length == 1)
+                return array[0] == qry ? 0 : -1;
+            if(array.Length == 2)
+            {
+                if (array[0] == qry) return 0;
+                if (array[1] == qry) return 1;
+                return -1;
+            }
+
+            var mid = array.Length / 2;
+            var midElement = array[mid];
+            if (midElement == qry)
+                return mid;
+            if(midElement > qry)
+            {
+                return binarySearchRecursive(qry, array.Take(mid).ToArray());
+            }
+
+            var index = binarySearchRecursive(qry, array.Skip(mid).ToArray());
+            if (index == -1) return -1;
+            return mid + index;
+        }
         static void Main(string[] args)
         {
             var elements = Enumerable.Range(0, 10).ToArray();
             var count = recursiveCont(elements);
             var max = maxRecursive(elements);
+            var s1 = binarySearchRecursive(9, elements);
+            var s2 = binarySearchRecursive(200, elements);
+            var s3 = binarySearchRecursive(0, elements);
+            var s5 = binarySearchRecursive(1, elements);
+            var s4 = binarySearchRecursive(-5, elements);
             var result = sum(elements);
         }
     }
