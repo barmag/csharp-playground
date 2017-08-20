38402eef2697d20b29b65050a98a073ef494232f
Yasser Makram <yasser@barm.ag>
2017-08-20 11:44:55 +0200

    depth first search

---
+++ grokking-algorithms/graph-dfs-bfs/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace graph_dfs_bfs
+{
+    class Graph
+    {
+        Dictionary<int, Node> nodeLookup = new Dictionary<int, Node>();
+        
+        public class Node
+        {
+            public int id;
+            public LinkedList<Node> adjacent = new LinkedList<Node>();
+            public Node(int id)
+            {
+                this.id = id;
+            }
+        }
+
+        private Node getNode(int id)
+        {
+            if (nodeLookup.ContainsKey(id))
+            {
+                return nodeLookup[id];
+            }
+
+            var n = new Node(id);
+            nodeLookup.Add(id, n);
+            return n;
+        }
+
+        public void addEdge(int source, int destination)
+        {
+            Node s = getNode(source);
+            Node d = getNode(destination);
+            s.adjacent.AddLast(d);
+        }
+
+        public bool hasPathDFS(int source, int dest)
+        {
+            var s = getNode(source);
+            var d = getNode(dest);
+            HashSet<int> visites = new HashSet<int>();
+            return hasPathDfsRecursive(s, d, visites);
+        }
+
+        private bool hasPathDfsRecursive(Node src, Node dest, HashSet<int> visited)
+        {
+            if (visited.Contains(src.id)) return false;
+            visited.Add(src.id);
+
+            if (src == dest)
+            {
+                return true;
+            }
+            foreach (Node child in src.adjacent)
+            {
+                if (hasPathDfsRecursive(child, dest, visited))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph g = new Graph();
+            g.addEdge(0, 1);
+            g.addEdge(0, 2);
+            g.addEdge(1, 3);
+            g.addEdge(3, 5);
+            g.addEdge(3, 6);
+            g.addEdge(6, 1);
+            var res = g.hasPathDFS(3, 2);
+            g.addEdge(5, 0);
+            res = g.hasPathDFS(3, 2);
+        }
+    }
+}
