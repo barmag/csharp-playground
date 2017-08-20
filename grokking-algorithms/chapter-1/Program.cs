acc581768c26eef26899882d24a6802dc4d3f553
Yasser Makram <yasser@barm.ag>
2017-08-20 11:18:26 +0200

    word permuttions count

--- grokking-algorithms/chapter-1/Program.cs
+++ grokking-algorithms/chapter-1/Program.cs
@@ -40,6 +40,35 @@ namespace chapter_1
             result = binarySearch( arr, -1);
             result = binarySearch(arr, 300);
             result = binarySearch(arr, 3000);
+
+            //how many variance a word ideal have
+            var a = countAnagrams("ab");
+            var a2 = countAnagrams("abc");
+            var a3 = countAnagrams("abcd");
+            var a4 = countAnagrams("IDEAL");
+        }
+
+        static int countAnagrams(string s)
+        {
+            Dictionary<string, int> variances = new Dictionary<string, int>();
+            variances.Add(s, 0);
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                var chars = s.ToCharArray();
+                var toSwitch = chars[i];
+                for (int j = 0; j < s.Length; j++)
+                {
+                    var tmp = chars[j];
+                    chars[j] = chars[i];
+                    chars[i] = tmp;
+                    string ns = string.Join("", chars);
+                    if (!variances.ContainsKey(ns))
+                        variances.Add(ns, 0);
+                }
+            }
+
+            return variances.Count;
         }
     }
 }
