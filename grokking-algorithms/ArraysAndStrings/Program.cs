cbdfa9a2fb7e829303ef02aeba4b611155b88b3b
Yasser Makram <yasser@barm.ag>
2017-09-15 18:59:51 +0200

    is unique characters in string with sorting

--- grokking-algorithms/ArraysAndStrings/Program.cs
+++ grokking-algorithms/ArraysAndStrings/Program.cs
@@ -11,6 +11,7 @@ namespace ArraysAndStrings
         /// <summary>
         /// Tests if a the characters in a string are unique
         /// Brute force implementation
+        /// O(n squared)
         /// </summary>
         /// <param name="word"></param>
         /// <returns></returns>
@@ -22,18 +23,43 @@ namespace ArraysAndStrings
             // Then there must be a repetiotion ... exit
             if (word.Length > max_length) return false;
 
-            var chars = word.ToCharArray();
+            //var chars = word.ToCharArray();
             int currentIndex = 0;
-            foreach(var c in chars)
+            foreach(var c in word)
             {
-                for (int i = 0; i < chars.Length; i++)
+                for (int i = 0; i < word.Length; i++)
                 {
-                    if (i != currentIndex && chars[i] == c)
+                    if (i != currentIndex && word[i] == c)
                     {
                         return false;
                     }
                 }
                 currentIndex++;
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Tests if characters in string is unique
+        /// Implementation with sorting o (n log(n))
+        /// </summary>
+        /// <param name="word"></param>
+        /// <returns></returns>
+        public static bool CharsInString_sorting(string word)
+        {
+            // assuming only english alpha characters are allowed
+            var max_length = ('z' - 'a') + ('Z' - 'A');
+            // if the length of the word is larger than the unique chars count
+            // Then there must be a repetiotion ... exit
+            if (word.Length > max_length || word.Length < 2) return false;
+            
+            var chars = word.OrderBy(c => c).ToArray();
+            for (int i = 0; i < chars.Length-1; i++)
+            {
+                if (chars[i] == chars[i+1])
+                {
+                    return false;
+                }
             }
             return true;
         }
@@ -47,6 +73,9 @@ namespace ArraysAndStrings
 
             Console.WriteLine("Hello world: {0}", IsUnique.CharsInString_Brute(c1));
             Console.WriteLine("unique: {0}", IsUnique.CharsInString_Brute(c2));
+
+            Console.WriteLine("Hello world: {0}", IsUnique.CharsInString_sorting(c1));
+            Console.WriteLine("unique: {0}", IsUnique.CharsInString_sorting(c2));
         }
     }
 }
