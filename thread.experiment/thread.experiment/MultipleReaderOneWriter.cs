0c09fa2f90294f36f50064ef478e52d858b54048
Yasser Makram <yasser@barm.ag>
2018-08-19 16:25:08 -0400

    Producer/Consumer trial 2. Multiple readers, single writer

--- thread.experiment/thread.experiment/MultipleReaderOneWriter.cs
+++ thread.experiment/thread.experiment/MultipleReaderOneWriter.cs
@@ -15,33 +15,62 @@ namespace thread.experiment
     public class MultipleReaderOneWriter
     {
         private readonly List<int> _data = new List<int>();
-        private const int DELAY = 1000;
+        private const int Delay = 1000;
+
+        private readonly object _counterSync = new object();
+        private int _resourceCounter = 0;
 
         // Exclusive lock ... one reader, and one writer
         public void Write()
         {
+            lock (_counterSync)
+            {
+                while (_resourceCounter != 0)
+                {
+                    Monitor.Wait(_counterSync);
+                }
+
+                _resourceCounter = -1;
+            }
             lock (_data)
             {
                 // Read data
                 int count = _data.Count;
                 // Simulate a slow write
                 Console.WriteLine("Beginning to write item No: {0}", count);
-                Thread.Sleep(DELAY);
+                Thread.Sleep(Delay);
                 _data.Add(count);
                 Console.WriteLine("Done writing: {0}", _data.Count);
+                _resourceCounter = 0;
             }
         }
 
-        private int _readers = 0;
         public int Read()
         {
-            lock (_data)
+            lock (_counterSync)
             {
-                Interlocked.Increment(ref _readers);
-                Console.WriteLine("readers: {0}", _readers);
-                Thread.Sleep(DELAY);
-                Interlocked.Decrement(ref _readers);
-                Console.WriteLine("end reading, reader: {0}", _readers);
+                while (_resourceCounter == -1)
+                {
+                    Monitor.Wait(_counterSync);
+                }
+
+                Interlocked.Increment(ref _resourceCounter);
+            }
+            
+            {
+                //Interlocked.Increment(ref _readers);
+                Console.WriteLine("readers: {0}", _resourceCounter);
+                Thread.Sleep(Delay);
+                Interlocked.Decrement(ref _resourceCounter);
+                Console.WriteLine("end reading, reader: {0}", _resourceCounter);
+                lock (_counterSync)
+                {
+                    if (_resourceCounter == 0)
+                    {
+                        Monitor.Pulse(_counterSync);
+                    }
+                }
+
                 return _data.Count;
             }
         }
