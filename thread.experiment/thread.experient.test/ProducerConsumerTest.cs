79e11f75d89383dbb8a9b85e1ffbe20bbdb330c0
Yasser Makram <yasser@barm.ag>
2018-08-19 15:11:49 -0400

    Producer consumer naive implementation Exclusive locking yields one reader, one writer at a time

---
+++ thread.experiment/thread.experient.test/ProducerConsumerTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using thread.experiment;
+
+namespace thread.experient.test
+{
+    [TestClass]
+    public class ProducerConsumerTest
+    {
+        [TestMethod]
+        public void VerifySequential()
+        {
+            var r = new Random();
+            var producerConsumer = new MultipleReaderOneWriter();
+            var count = 0;
+            var readers = new List<Thread>();
+            for (int i = 0; i < 10; i++)
+            {
+                var reader = new Thread(() =>
+                {
+                    count = producerConsumer.Read();
+                });
+                reader.Start();
+                readers.Add(reader);
+                Thread.Sleep(r.Next(1, 50));
+            }
+            var writer = new Thread(() =>
+            {
+                producerConsumer.Write();
+            });
+
+            writer.Start();
+            writer.Join();
+            foreach (var reader in readers)
+            {
+                reader.Join();
+            }
+
+            count = producerConsumer.Read();
+            Assert.AreEqual(1, count);
+        }
+    }
+}
---
+++ thread.experiment/thread.experiment/MultipleReaderOneWriter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace thread.experiment
+{
+    /// <summary>
+    /// Producer consumer thread safety problem
+    /// Handle the case where multiple threads can have:
+    /// - One writer can access the shared data
+    /// - Multiple readers can access the data simultaneously
+    /// - Readers cannot access the data while being written
+    /// </summary>
+    public class MultipleReaderOneWriter
+    {
+        private readonly List<int> _data = new List<int>();
+        private const int DELAY = 1000;
+
+        // Exclusive lock ... one reader, and one writer
+        public void Write()
+        {
+            lock (_data)
+            {
+                // Read data
+                int count = _data.Count;
+                // Simulate a slow write
+                Console.WriteLine("Beginning to write item No: {0}", count);
+                Thread.Sleep(DELAY);
+                _data.Add(count);
+                Console.WriteLine("Done writing: {0}", _data.Count);
+            }
+        }
+
+        private int _readers = 0;
+        public int Read()
+        {
+            lock (_data)
+            {
+                Interlocked.Increment(ref _readers);
+                Console.WriteLine("readers: {0}", _readers);
+                Thread.Sleep(DELAY);
+                Interlocked.Decrement(ref _readers);
+                Console.WriteLine("end reading, reader: {0}", _readers);
+                return _data.Count;
+            }
+        }
+    }
+}
